7f2aa1e32615b3ae3e890066f311f6fe93ef2533
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2020-02-23 15:25:57 +0530

    Initial commit for functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IAppProps { }
+
+// interface IAppState {
+//     name: string | null;
+// }
+
+interface ResponseType {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+// export default class App extends React.Component<IAppProps, IAppState> {
+
+const App: React.SFC<IAppProps> = props => {
+
+    const [name, setName] = useState<string>('');
+    // constructor(props: IAppProps) {
+    //     super(props);
+    //     this.state = { name: null };
+    // }
+
+    // componentDidMount = async (): Promise<void> => {
+    //     const data: ResponseType = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //         .then(response => response.json())
+
+    //     this.setState({ name: data.title });
+
+    // }
+
+    const getName = async () => {
+        const data: ResponseType = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+
+        setName(data.title);
+    }
+
+    useEffect(() => {getName()})
+
+
+    return (
+        <div className="App">
+            Hello Hooks &nbsp; <b>{name}</b>
+        </div>
+    );
 }
 
 export default App;
+
